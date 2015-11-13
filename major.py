c8b536e7bb00705987428e238549eb35de3420df
Alexandra <hiten-hemyra@mail.ru>
2015-11-14 00:42:49 +0300

    Create major.py

---
+++ major.py
@@ -0,0 +1,18 @@
+def major(a):
+    e = a[0]
+    count = 1
+    for i in range(1, len(a)):
+        if a[i] == e:
+            count += 1
+        else:
+            count -= 1
+            if count == -1:
+                e = a[i]
+                count = 1
+    n = 0
+    for x in a:
+        if e == x:
+            n += 1
+    if n < len(a)/2:
+        e = None
+    print(e)
