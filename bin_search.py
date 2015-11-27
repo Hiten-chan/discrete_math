de7b0d5af42f40081da78cbccd22e419e23da7cc
Alexandra <hiten-hemyra@mail.ru>
2015-11-28 02:39:04 +0300

    Update bin_search.py

--- bin_search.py
+++ bin_search.py
@@ -3,16 +3,20 @@
 def bin_search(a, key):
     l = 0
     r = int(len(a) + 1)
-    while l < r - 1:              
-        m = int((l + r) / 2)            
-        if a[m] < key:
-            l = m
-        else: 
-            r = m 
-
-    if a[r] == key:
-        return r     
-    elif a[0] == key:
-        return 0
-    else:
+    
+    if key > a[int(len(a))-1]:
         return 'Not found'
+    else:
+        while l < r - 1:              
+            m = int((l + r) / 2)            
+            if a[m] < key:
+                l = m
+            else: 
+                r = m 
+
+        if a[r] == key:
+            return r     
+        elif a[0] == key:
+            return 0
+        else:
+            return 'Not found'
